a8b015520aeff9bed8a0344b014e30defc2fa3d7
Mayatskiy Anton <work.mayatskiy@gmail.com>
2024-06-17 17:35:05 +1000

    feat: add primary product

--- src/api/product/services/product.ts
+++ src/api/product/services/product.ts
@@ -4,4 +4,38 @@
 
 import { factories } from '@strapi/strapi';
 
-export default factories.createCoreService('api::product.product');
+export default factories.createCoreService('api::product.product', ({strapi}) => ({
+  async findPublic(query: any) {
+    const newQuery = {
+      ...query,
+      filters: {
+        ...query.filters,
+        primary: false
+      }
+    };
+
+    //@ts-ignore
+    return await strapi.entityService.findMany("api::product.product", this.getFetchParams(newQuery));
+  },
+
+  // async findPublicOne(id: number, params = {}) {
+  //   const product = await strapi.service("api::product.product").findOne(id);
+  //
+  //   if (product.primary == true) return ctx.forbidden('You do not have access to this product');
+  //
+  //   const sanitizedProduct = await this.sanitizeOutput(product, ctx);
+  //
+  //   return this.transformResponse(sanitizedProduct);
+  // }
+
+  async findOneIfPublic(args) {
+    const {id, query} = args;
+
+    //@ts-ignore
+    const post = await strapi.entityService.findOne("api::product.product", id, this.getFetchParams(query));
+
+    return post.primary ? null : post;
+  }
+
+}));
+
