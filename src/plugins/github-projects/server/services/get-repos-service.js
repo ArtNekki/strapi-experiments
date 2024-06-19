a83ae7496ec046cb29d1966f419b2a658749542f
Mayatskiy Anton <work.mayatskiy@gmail.com>
2024-06-19 22:16:31 +1000

    feat: fetch github projects

--- src/plugins/github-projects/server/services/get-repos-service.js
+++ src/plugins/github-projects/server/services/get-repos-service.js
@@ -1,7 +1,16 @@
 'use strict';
+// import { request } from "@octokit/request";
+ const { request } = require("@octokit/request");
 
 module.exports = ({ strapi }) => ({
-  getPublicRepos() {
-    return 'Welcome to Strapi 🚀';
+   getPublicRepos: async () => {
+    const result = await request("GET /user/repos", {
+      headers: {
+        authorization: `token ${process.env.GITHUB_TOKEN}`,
+      },
+      type: "public",
+    });
+
+    return result;
   },
 });
