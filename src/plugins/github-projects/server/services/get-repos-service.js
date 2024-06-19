a27ed778accded9681951f722c955e9b98f21560
Mayatskiy Anton <work.mayatskiy@gmail.com>
2024-06-19 22:58:29 +1000

    feat: format data

--- src/plugins/github-projects/server/services/get-repos-service.js
+++ src/plugins/github-projects/server/services/get-repos-service.js
@@ -1,6 +1,7 @@
 'use strict';
 // import { request } from "@octokit/request";
- const { request } = require("@octokit/request");
+const { request } = require("@octokit/request");
+const axios = require("axios");
 
 module.exports = ({ strapi }) => ({
    getPublicRepos: async () => {
@@ -11,6 +12,30 @@ module.exports = ({ strapi }) => ({
       type: "public",
     });
 
-    return result;
+    //https://raw.githubusercontent.com/ArtNekki/strapi-experiments/main/README.md
+    //
+     return Promise.all(result.data.map(async (project) => {
+      const {id, name, description, html_url, owner, default_branch} = project;
+      const readmeUrl = `https://raw.githubusercontent.com/${owner.login}/${name}/${default_branch}/README.md`;
+      console.log('readmeUrl', readmeUrl);
+
+      let longDescription = '';
+
+      try {
+        const response = await axios.get(readmeUrl);
+        longDescription = response.data;
+      } catch (error) {
+        if (error.response && error.response.status === 404) {
+          console.error(`README.md not found for project: ${name}`);
+          longDescription = 'README.md not found';
+        } else {
+          console.error(`Error fetching README.md for project: ${name}`, error);
+          longDescription = 'Error fetching README.md';
+        }
+      }
+
+      return {id, name, shortDescription: description, url: html_url, longDescription};
+
+    }));
   },
 });
