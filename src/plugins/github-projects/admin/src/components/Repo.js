45496af1e900df7f7c00dfaade0c8cfeaf6b9e32
Mayatskiy Anton <work.mayatskiy@gmail.com>
2024-06-20 22:36:08 +1000

    fix: hot fix

--- src/plugins/github-projects/admin/src/components/Repo.js
+++ src/plugins/github-projects/admin/src/components/Repo.js
@@ -1,6 +1,6 @@
 import React, {useEffect, useState} from 'react';
 import { Table, Thead, Tbody, Tr, Td, Th } from '@strapi/design-system/Table';
-import { Box, Checkbox, Typography, Flex, IconButton, Alert, Loader } from '@strapi/design-system';
+import { Box, Checkbox, Typography, Flex, IconButton, Alert, Loader, Link } from '@strapi/design-system';
 import { Pencil, Trash } from '@strapi/icons';
 import axios from "../utils/axiosInstance";
 
@@ -14,36 +14,18 @@ const Repo = () => {
     const fetchData = async () => {
       setLoading(true);
 
-      await axios.get('/github-projects/repos')
-        .then((result) => {
-          return setRepos(result.data);
-        })
-        .catch((error) => {
-          return setError(error);
-        });
+      try {
+        const result = await axios.get('/github-projects/repos');
+        setRepos(result.data);
+      } catch (error) {
+        setError(error);
+      } finally {
+        setLoading(false);
+      }
     };
 
     fetchData();
-
-    setLoading(false);
   }, []);
-
-
-  // useEffect(async () => {
-  //   setLoading(true);
-  //
-  //   // axios
-  //   //   .get('/github-projects/repos')
-  //   //   .then((response) => {
-  //   //     console.log('setRepos(response.data)', setRepos(response.data))
-  //   //     return setRepos(response.data)
-  //   //   })
-  //   //   .catch((error) => {
-  //   //     console.log('error', error);
-  //   //     return setError(error);
-  //   //   })
-  //   setLoading(false);
-  // });
 
   if (error) {
     return <Alert closeLabel="Close" title="Error fetching repositories" variant="danger">
@@ -76,32 +58,40 @@ const Repo = () => {
           </Tr>
         </Thead>
         <Tbody>
-          {repos.map(entry => <Tr key={entry.id}>
-            <Td>
-              <Checkbox aria-label={`Select ${entry.contact}`} />
-            </Td>
-            <Td>
-              <Typography textColor="neutral800">{entry.name}</Typography>
-            </Td>
-            <Td>
-              <Typography textColor="neutral800">{entry.shortDescription}</Typography>
-            </Td>
-            <Td>
-              <Typography textColor="neutral800">{entry.url}</Typography>
-            </Td>
-            <Td>
-              <Flex>
-                <IconButton onClick={() => console.log('edit')} label="Edit" borderWidth={0}>
-                  <Pencil />
-                </IconButton>
-                <Box paddingLeft={1}>
-                  <IconButton onClick={() => console.log('delete')} label="Delete" borderWidth={0}>
-                    <Trash />
-                  </IconButton>
-                </Box>
-              </Flex>
-            </Td>
-          </Tr>)}
+          {repos.map((repo) => {
+            const {id, name, shortDescription, url, projectId} = repo;
+
+            return (
+              <Tr key={id}>
+                <Td>
+                  <Checkbox aria-label={`Select ${id}`} />
+                </Td>
+                <Td>
+                  <Typography textColor="neutral800">{name}</Typography>
+                </Td>
+                <Td>
+                  <Typography textColor="neutral800">{shortDescription}</Typography>
+                </Td>
+                <Td>
+                  <Typography textColor="neutral800">
+                    <Link href={url} isExternal>{url}</Link>
+                  </Typography>
+                </Td>
+                <Td>
+                  <Flex>
+                    <IconButton onClick={() => console.log('edit')} label="Edit" borderWidth={0}>
+                      <Pencil />
+                    </IconButton>
+                    <Box paddingLeft={1}>
+                      <IconButton onClick={() => console.log('delete')} label="Delete" borderWidth={0}>
+                        <Trash />
+                      </IconButton>
+                    </Box>
+                  </Flex>
+                </Td>
+              </Tr>
+            )
+        })}
         </Tbody>
       </Table>
     </Box>
